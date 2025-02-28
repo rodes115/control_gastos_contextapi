571cebb9696aa3d0666c8a2ed34da5c2be415e09
rodes115 <rodesreyes115@gmail.com>
2025-02-27 22:57:07 -0600

    Primer Commit Capitulo 202

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from "react"
+import BudgedForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
+
+
+function App() {
+
+  const {state} = useBudget()
+  
+  const isValidBudget = useMemo (() => state.budget > 0, [state.budget])
+
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <div className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </div>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget ? <BudgetTracker/> : <BudgedForm/>}
+      </div>
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        
+        </main>
+      )}
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/context/BudgedContext.tsx
@@ -0,0 +1,32 @@
+import { useReducer, createContext, Dispatch, ReactNode  } from "react"
+import { budgetReducer, BudgetState, initialState, BudgetActions} from "../reducers/budget-reducer"
+
+type BudgetContextProps = {
+     state: BudgetState
+     dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps={
+     children:ReactNode
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider = ({children} : BudgetProviderProps) => {
+
+     const [state,dispatch] = useReducer(budgetReducer,initialState)
+
+     return(
+     
+          <BudgetContext.Provider
+               value={{
+                    state,
+                    dispatch
+               }}
+          >
+               {children}
+          </BudgetContext.Provider>
+
+     )
+
+}
